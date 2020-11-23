f1cc6cab2f581fc060b576f4927ad8de4fd69158
gabriele.salvato <gabriele.salvato@cnr.it>
2020-11-23 13:25:50 +0100

    working

---
+++ OnePulseMode/Inc/main.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f4xx_hal.h"
+
+void Error_Handler(void);
+
+// Onboard Push Button
+#define B1_Pin       GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+
+// USART connected to USB
+#define USART_TX_Pin       GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin       GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+
+// Onboard Led
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+
+// Timer Pins
+#define leftPWM_Pin GPIO_PIN_6
+#define leftPWM_GPIO_Port GPIOA
+#define rightPWM_Pin GPIO_PIN_7
+#define rightPWM_GPIO_Port GPIOA
+
+// JTAG Pins
+#define TMS_Pin       GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin       GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin       GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ OnePulseMode/Src/main.cpp
@@ -0,0 +1,250 @@
+#include "main.h"
+#include "string.h" // for memset()
+
+
+TIM_HandleTypeDef  htim3;
+UART_HandleTypeDef huart2;
+
+
+static void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_USART2_UART_Init(void);
+
+double timerClockFrequency = 1.0e7;  // 10 MHz
+double pulseDelay          = 1.0e-6; // in seconds
+double pulsewidth          = 1.0e-6; // in seconds
+
+
+int
+main(void) {
+    HAL_Init();
+    SystemClock_Config();
+
+    MX_GPIO_Init(); // led on PA5 - CN10 --> Pin11
+    MX_TIM3_Init();
+    MX_USART2_UART_Init();
+
+    HAL_Delay(3000);
+    if(HAL_TIM_OnePulse_Start(&htim3, TIM_CHANNEL_1) != HAL_OK) {
+      Error_Handler();
+    }
+    while(1) {
+        HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+        HAL_Delay(1);
+        HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+        HAL_Delay(10);
+    }
+}
+
+
+//                              ____
+//                              |   |
+// CH2 _________________________|   |_________________________________________
+//
+//                                            ___________________________
+//                                           |                           |
+// CH1 ______________________________________|                           |____
+//                             <---Delay----><------Pulse--------------->
+//
+void
+MX_TIM3_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct;
+    memset(&GPIO_InitStruct, 0, sizeof(GPIO_InitStruct));
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    // TIM3 GPIO Configuration
+    //    PA6 ------> TIM3_CH1 (Output CN10 - 13)
+    //    PA7 ------> TIM3_CH2 (Input  CN10 - 15)
+    GPIO_InitStruct.Pin       = GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_PULLUP;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    uint32_t uwPrescalerValue = (uint32_t) (SystemCoreClock/timerClockFrequency)-1;
+    uint16_t PulseWidthNumber = pulsewidth*timerClockFrequency;
+    uint16_t PulseDelayNumber = pulseDelay*timerClockFrequency;
+    uint16_t period = PulseWidthNumber+PulseDelayNumber;
+
+    __HAL_RCC_TIM3_CLK_ENABLE();
+    memset(&htim3, 0, sizeof(htim3));
+    htim3.Instance = TIM3;
+    htim3.Init.Prescaler         = uwPrescalerValue;
+    htim3.Init.CounterMode       = TIM_COUNTERMODE_UP;
+    htim3.Init.Period            = period; // Pulse Total
+    htim3.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;
+    htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    if(HAL_TIM_OnePulse_Init(&htim3, TIM_OPMODE_SINGLE) != HAL_OK) {
+        Error_Handler();
+    }
+
+    TIM_OnePulse_InitTypeDef sConfig;
+    memset(&sConfig, 0, sizeof(sConfig));
+    //sConfig.OCMode       = TIM_OCMODE_TIMING;
+    sConfig.OCMode       = TIM_OCMODE_PWM2;
+    sConfig.Pulse        = PulseDelayNumber;
+    sConfig.OCPolarity   = TIM_OCPOLARITY_HIGH;
+    sConfig.OCNPolarity  = TIM_OCNPOLARITY_HIGH;
+    sConfig.OCIdleState  = TIM_OCIDLESTATE_RESET;
+    sConfig.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+    sConfig.ICPolarity   = TIM_ICPOLARITY_RISING;
+    sConfig.ICSelection  = TIM_ICSELECTION_DIRECTTI;
+    sConfig.ICFilter     = 0;
+    if(HAL_TIM_OnePulse_ConfigChannel(&htim3, &sConfig, TIM_CHANNEL_1, TIM_CHANNEL_2) != HAL_OK) {
+      Error_Handler();
+    }
+/*
+    // Clock Source is internal
+    TIM_ClockConfigTypeDef  sClockSourceConfig;
+    memset(&sClockSourceConfig, 0, sizeof(sClockSourceConfig));
+    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+    if(HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK) {
+        Error_Handler();
+    }
+
+    // No Master-Slave Mode
+    TIM_MasterConfigTypeDef sMasterConfig;
+    memset(&sMasterConfig, 0, sizeof(sMasterConfig));
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    if(HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK) {
+        Error_Handler();
+    }
+*/
+}
+
+
+void
+MX_USART2_UART_Init(void) {
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate     = 9600;
+    huart2.Init.WordLength   = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits     = UART_STOPBITS_1;
+    huart2.Init.Parity       = UART_PARITY_NONE;
+    huart2.Init.Mode         = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart2) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+
+
+void
+MX_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct;
+    memset(&GPIO_InitStruct, 0, sizeof(GPIO_InitStruct));
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+    GPIO_InitStruct.Pin = B1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LD2_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+}
+
+
+void
+HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base) {
+    if(htim_base->Instance==TIM3) {
+        __HAL_RCC_TIM3_CLK_DISABLE();
+    }
+}
+
+
+void
+Error_Handler(void) {
+
+}
+
+
+// This function handles EXTI line[15:10] interrupts.
+void
+EXTI15_10_IRQHandler(void) { // defined in file "startup_stm32f411xe.s"
+    // Non dovremmo controllare l'origine dell'interrupt ???
+    HAL_GPIO_EXTI_IRQHandler(B1_Pin);
+}
+
+
+void
+HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    if(GPIO_Pin == B1_Pin) {
+
+    }
+}
+
+
+
+// System Clock Configuration
+//    The system Clock is configured as follow :
+//        System Clock source            = PLL (HSI)
+//        SYSCLK(Hz)                     = 100000000
+//        HCLK(Hz)                       = 100000000
+//        AHB Prescaler                  = 1
+//        APB1 Prescaler                 = 2
+//        APB2 Prescaler                 = 1
+//        HSI Frequency(Hz)              = 16000000
+//        PLL_M                          = 16
+//        PLL_N                          = 400
+//        PLL_P                          = 4
+//        PLL_Q                          = 7
+//        VDD(V)                         = 3.3
+//        Main regulator output voltage  = Scale2 mode
+//        Flash Latency(WS)              = 3
+void
+SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    memset(&RCC_OscInitStruct, 0, sizeof(RCC_OscInitStruct));
+    memset(&RCC_ClkInitStruct, 0, sizeof(RCC_ClkInitStruct));
+
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM            = 16;
+    RCC_OscInitStruct.PLL.PLLN            = 400;
+    RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV4;
+    RCC_OscInitStruct.PLL.PLLQ            = 7;
+    if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+
+    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK   |
+                                       RCC_CLOCKTYPE_SYSCLK |
+                                       RCC_CLOCKTYPE_PCLK1  |
+                                       RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+
+#ifdef  USE_FULL_ASSERT
+void
+assert_failed(uint8_t *file, uint32_t line) {
+    //  User can add his own implementation to report the file name and line number,
+    //     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line)
+}
+#endif /* USE_FULL_ASSERT */
